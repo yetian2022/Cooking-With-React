d7abccec686837f27dbecbb6dbe8b9e5cc50223b
Ye Tian <yetian@Yes-MacBook-Pro.local>
2022-08-02 06:35:17 -0400

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react"
+import { ThemeContext } from "./App"
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: props.initialCount,
+    }
+  }
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(style) => (
+          <div>
+            <button style={style} onClick={() => this.changeCount(-1)}>
+              -
+            </button>
+            <span>{this.state.count}</span>
+            <button style={style} onClick={() => this.changeCount(+1)}>
+              +
+            </button>
+          </div>
+        )}
+      </ThemeContext.Consumer>
+    )
+  }
+
+  changeCount(amount) {
+    this.setState((preState) => {
+      return { count: preState.count + amount }
+    })
+  }
+}
